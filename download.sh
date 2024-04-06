6ecdc8a15a7bbffcf18597af3e1682440b67211a
program think backup <103631137+hello-world-1989@users.noreply.github.com>
2024-04-06 01:46:48 +0000

    add tweet json

---
+++ download.sh
@@ -0,0 +1,20 @@
+wget -O 202403.zip https://github.com/hello-world-1989/whyyoutouzhele/releases/download/202403/whyyoutouzhele.zip
+wget -O 202402.zip https://github.com/hello-world-1989/whyyoutouzhele/releases/download/202402/whyyoutouzhele.zip
+wget -O 202401.zip https://github.com/hello-world-1989/whyyoutouzhele/releases/download/202401/whyyoutouzhele.zip
+
+wget -O 202312.zip https://github.com/hello-world-1989/whyyoutouzhele/releases/download/202312/whyyoutouzhele.zip
+wget -O 202311.zip https://github.com/hello-world-1989/whyyoutouzhele/releases/download/202311/whyyoutouzhele.zip
+wget -O 202310.zip https://github.com/hello-world-1989/whyyoutouzhele/releases/download/202310/whyyoutouzhele.zip
+wget -O 202309.zip https://github.com/hello-world-1989/whyyoutouzhele/releases/download/202309/whyyoutouzhele.zip
+wget -O 202308.zip https://github.com/hello-world-1989/whyyoutouzhele/releases/download/202308/whyyoutouzhele.zip
+wget -O 202307.zip https://github.com/hello-world-1989/whyyoutouzhele/releases/download/202307/whyyoutouzhele.zip
+wget -O 202306.zip https://github.com/hello-world-1989/whyyoutouzhele/releases/download/202306/whyyoutouzhele.zip
+wget -O 202305.zip https://github.com/hello-world-1989/whyyoutouzhele/releases/download/202305/whyyoutouzhele.zip
+wget -O 202304.zip https://github.com/hello-world-1989/whyyoutouzhele/releases/download/202304/whyyoutouzhele.zip
+wget -O 202303.zip https://github.com/hello-world-1989/whyyoutouzhele/releases/download/202303/whyyoutouzhele.zip
+wget -O 202302.zip https://github.com/hello-world-1989/whyyoutouzhele/releases/download/202302/whyyoutouzhele.zip
+wget -O 202301.zip https://github.com/hello-world-1989/whyyoutouzhele/releases/download/202301/whyyoutouzhele.zip
+
+wget -O 202212.zip https://github.com/hello-world-1989/whyyoutouzhele/releases/download/202212/whyyoutouzhele.zip
+wget -O 202211.zip https://github.com/hello-world-1989/whyyoutouzhele/releases/download/202211/whyyoutouzhele.zip
+wget -O 202210.zip https://github.com/hello-world-1989/whyyoutouzhele/releases/download/202210/whyyoutouzhele.zip
